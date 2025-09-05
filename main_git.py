3c154a1144ebfbf52dd5f0fb2b0883a3cc1e682b
Edwin ASplund <edwinasplund.06@gmail.com>
2025-09-05 11:01:51 +0200

    randomizer

--- main_git.py
+++ main_git.py
@@ -1,1 +1,17 @@
 print("hello world")
+
+import random
+
+l=[]
+for i in range(10):
+    y = random.randint(1,6)
+    l.append(y)
+print(l)
+
+print("monkey")
+
+x = random.randint(1,6)
+print(x)
+
+
+
